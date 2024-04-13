c3422bc442b3199f8c3776fd8449939bb3bcfb50
Pablogs989 <pablogonsan2001@gmail.com>
2024-04-13 19:09:09 +0200

    users

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
+const nameElement = document.getElementById("name");
+const dateElement = document.getElementById("date");
 const apiKey = 'KFGmOUvBmwkfPjrKCJZWTdlMVJbJSX0soimMSV5a';
 const limit = 20;
+const date = new Date();
+const formatedDate = date.toISOString().split('T')[0];
+dateElement.value = formatedDate;
 
 let questions = [];
 let filteredQuestions = [];
 let marks = 0;
+let currentQuestionIndex;
+let stats = JSON.parse(localStorage.getItem('stats')) || [];
 
 const filterQuestions = (questions) => {
   for (const question in questions) {
@@ -53,12 +60,21 @@ const getQuestions = () => {
   filteredQuestions = [];
   apiAccess();
 }
-getQuestions();
 
-let currentQuestionIndex;
+const createUser = () => {
+  let user = {
+    name: nameElement.value,
+    date: formatedDate,
+    marks: marks
+  }
+  stats.push(user);
+  localStorage.setItem("stats", JSON.stringify(stats));
+}
 
 const startGame = () => {
   startButton.classList.add("hide");
+  nameElement.classList.add("hide");
+  dateElement.classList.add("hide");
   currentQuestionIndex = 0;
   marks = 0;
   questionContainerElement.classList.remove("hide");
@@ -102,7 +118,6 @@ const setStatusClass = (element) => {
   }
 }
 
-
 const selectAnswer = (event) => {
   const selectedButton = event.target;
   if (selectedButton.dataset.correct) {
@@ -114,13 +129,19 @@ const selectAnswer = (event) => {
   if (filteredQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove("hide");
   } else {
-    startButton.innerText = "Restart";
-    startButton.classList.remove("hide");
-    answerButtonsElement.innerHTML = "";
-    questionElement.innerHTML = "";
-
+    createUser();
+    setTimeout(() => {
+      startButton.innerText = "Restart";
+      startButton.classList.remove("hide");
+      nameElement.classList.remove("hide");
+      dateElement.classList.remove("hide");
+      answerButtonsElement.innerHTML = "";
+      questionElement.innerHTML = "";
+    }, 2000);
   }
 }
+
+getQuestions();
 
 startButton.addEventListener("click", startGame);
 nextButton.addEventListener("click", () => {
